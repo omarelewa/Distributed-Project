d1fdbd8c2b4e58af72556d9d5849a21039d43949
omar <elewa97@aucegypt.edu>
2022-11-30 18:16:00 +0200

    ~

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,35 @@
 // cargo run 127.0.0.1:5656 127.0.0.1:7878 1
 
 
-mod receiver;
-mod sender;
-use std::thread;
-use std::env;
-use receiver::RequestReceiver;
-use sender::RequestSender;
+mod receiver; // import receiver module
+mod sender; // import sender module
+use std::thread; // import thread module
+use std::env; // import env module
+use receiver::RequestReceiver; // import RequestReceiver struct
+use sender::RequestSender; // import RequestSender struct
 fn main() {
     let args: Vec<String> = env::args().collect();  // get local and remote addresses
-    let send_addr = &args[1];
-    let receive_addr = &args[2];
-    let remote = &args[3];
+    let send_addr = &args[1]; // get local address
+    let receive_addr = &args[2]; // get remote address
+    let remote = &args[3]; // get remote flag (0 if server, 1 if client)
     
-    let receiver = RequestReceiver::new(receive_addr.to_string());
-    let sender = RequestSender::new(send_addr.to_string(), remote.to_string());
-    let t1 = thread::spawn(
-        move || {
-            receiver.listen()
-        }
-    );
-    let t2 =thread::spawn(
-        move || {
-            sender.run()
-        }
-    );
+    let receiver = RequestReceiver::new(receive_addr.to_string()); // create receiver with receive_addr as address
+    let sender = RequestSender::new(send_addr.to_string(), remote.to_string()); // create sender with send_addr as address
 
-    t1.join().unwrap();
-    t2.join().unwrap();
+    let t1 = thread::spawn( // spawn thread to listen for requests
+        move || { // move receiver into thread
+            receiver.listen() // call listen function
+        } // end move
+    ); // end spawn
+
+    let t2 =thread::spawn( // spawn thread to send requests
+        move || { // move sender into thread
+            sender.run() // call run function
+        } // end move
+    ); // end spawn
+
+    t1.join().unwrap(); // join t1 thread to main thread 
+    t2.join().unwrap(); // join t2 thread to main thread
     
 }
 
